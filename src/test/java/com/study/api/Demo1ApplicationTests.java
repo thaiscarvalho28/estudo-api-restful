d2a2263f817194dd2df0cb10d644c2c7e21c17c7
Thais Carvalho <thaiscarvalho2898@gmail.com>
2020-10-26 11:02:09 -0300

    Iniciando o projeto de estudos de API.

---
+++ src/test/java/com/study/api/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
